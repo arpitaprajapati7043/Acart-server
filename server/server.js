65615c0ad1193568821706eb45b6ce67f592d864
arpita <arpitap433@gmail.com>
2023-12-28 09:17:31 -0800

    hh

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const helmet = require('helmet');
-const  dotenv=require("dotenv");
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const cors=require('cors');
-const cookieParser=require('cookie-parser');
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 const dbConnection = require('./dbConnection');
 const router = require('./routes/router');
 const userRouter = require('./routes/userRouter');
@@ -12,47 +12,26 @@ const cartRouter = require('./routes/cartRouter');
 const requestLogger = require('./utils/requestLogger');
 const errorHandler = require('./utils/errorHandler');
 
-
-
 const allowedOrigins = ['http://localhost:3000', 'https://acart-server.onrender.com'];
-
 
 app.use(cors({
   origin: allowedOrigins,
   credentials: true
 }));
 
-// app.use(cors((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   // other headers...
-//   next();
-// }));
-
-
-
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-//   res.header('Access-Control-Allow-Credentials', true);
-//   // other headers...
-//   next();
-// });
-
-
-// Specify middleware
 app.use(helmet.contentSecurityPolicy({
   useDefaults: true,
   directives: {
     defaultSrc: ["'self'"],
-    scriptSrc: ["'self'", "'unsafe-inline'", "'unsafe-eval'", 'https://apis.google.com'],
+    scriptSrc: ["'self'", 'https://apis.google.com'],
+    styleSrc: ["'self'", 'https://fonts.googleapis.com'], // Add other style sources
     // Add other directives as needed
   },
 }));
 
 app.use(express.json());
 app.use(cookieParser());
-
-app.use(requestLogger); // Make sure requestLogger middleware is defined and required
+app.use(requestLogger);
 app.use('/router', router);
 app.use('/user', userRouter);
 app.use('/cart', cartRouter);
@@ -61,5 +40,5 @@ app.use(errorHandler);
 const PORT = 8080;
 
 app.listen(PORT, () => {
-  console.group('server started on Port 8080');
+  console.group('Server started on Port 8080');
 });
